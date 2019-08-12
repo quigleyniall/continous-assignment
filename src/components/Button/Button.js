e3518438b88f607d769ed04ee71a0e21f48aaf8f
Niall Quigley <niallquigley92@gmail.com>
2019-08-12 12:56:14 +0100

    Composable Button

---
+++ src/components/Button/Button.js
@@ -0,0 +1,48 @@
+// @flow
+import React from 'react';
+import './Button.scss';
+
+type ButtonProps = {
+	onPress: () => void,
+	type: string,
+	text?: string,
+	icon?: string,
+	className: string,
+	disabled: boolean,
+	clicked: boolean,
+};
+
+const Button = ({
+	onPress,
+	type,
+	text,
+	icon,
+	className,
+	disabled,
+	clicked,
+}: ButtonProps) => (
+	<button
+		onClick={onPress}
+		className={
+			disabled
+				? `btn btn-${className} btn-disabled`
+				: `btn btn-${className}`
+		}
+		disabled={disabled}
+		type={type}>
+		{icon ? (
+			<span style={text ? { paddingRight: '5px' } : { paddingRight: 0 }}>
+				<i className={icon} />
+			</span>
+		) : null}
+		{clicked ? <span className="btn-clicked">text</span> : text}
+	</button>
+);
+
+Button.defaultProps = {
+	disabled: false,
+	className: 'btn',
+	clicked: false,
+};
+
+export default Button;
