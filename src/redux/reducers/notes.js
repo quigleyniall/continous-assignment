e6bbbf3a6e893856de0babab5574e02bc9941dbe
Niall Quigley <niallquigley92@gmail.com>
2019-08-12 11:51:16 +0100

    Notes reducers

---
+++ src/redux/reducers/notes.js
@@ -0,0 +1,145 @@
+import {
+	fetchAllNotes,
+	fetchNote,
+	createNewNote,
+	updateNote,
+	errorHandler,
+} from '../actions';
+
+type Note = {
+	id: number,
+	title: string,
+	note: string,
+};
+
+type State = {
+	notes: Array<Note>,
+	selectedNote: Note | null,
+	selectedNoteId: number | null,
+	error: boolean | string,
+};
+
+const initialState = {
+	notes: [
+		{
+			id: 2,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+		{
+			id: 1,
+			title: 'some title',
+			note: 'lorem  ksjbkjhg jkdg khgkjfsdh dbd kjdg g hkg ',
+		},
+	],
+	selectedNote: null,
+	selectedNoteId: null,
+	error: false,
+};
+
+export const notesReducer = (state: State = initialState, action) => {
+	switch (action.type) {
+		case fetchAllNotes:
+			return { ...state, notes: action.payload, error: false };
+		case fetchNote:
+			return {
+				...state,
+				selectedNote: state.notes.filter(
+					note => note.id === action.payload,
+				),
+			};
+		case createNewNote:
+			return { ...state, error: false };
+		case errorHandler:
+			return { ...state, error: action.payload };
+		case updateNote:
+			return { ...state, error: false };
+		default:
+			return state;
+	}
+};
