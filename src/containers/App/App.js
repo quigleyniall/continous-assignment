3b3b687437fc7c75a831eaa0a864ccbb135b2e60
Niall Quigley <niallquigley92@gmail.com>
2019-08-13 17:40:37 +0100

    Refactor App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,32 +1,20 @@
 //  @flow
 
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { createNote } from '../../redux/actions';
 import NoteContainer from '../NoteContainer/NoteContainer';
 import NoteInputContainer from '../NoteInputContainer/NoteInputContainer';
 import Header from '../../components/Header/Header';
-import Button from '../../components/Button/Button';
 import './App.scss';
 
-type State = {
-	createNewNote: boolean,
-};
-
-class App extends React.Component<null, State> {
-	state = {
-		createNewNote: false,
-	};
-
+class App extends React.Component<null> {
 	createNote = () => {
-		console.log('clicked');
-		this.setState({ createNewNote: true });
-	};
-
-	saveNote = () => {
-		console.log('saved');
+		const { createNewNote } = this.props;
+		createNewNote();
 	};
 
 	render() {
-		const { createNewNote } = this.state;
 		return (
 			<div className="wrapper">
 				<Header createNote={this.createNote} />
@@ -34,18 +22,12 @@ class App extends React.Component<null, State> {
 					<NoteContainer />
 					<NoteInputContainer />
 				</div>
-				<div className="save-button">
-					<Button
-						icon="far fa-save"
-						className="circle"
-						type="button"
-						onPress={this.saveNote}
-						disabled
-					/>
-				</div>
 			</div>
 		);
 	}
 }
 
-export default App;
+export default connect(
+	null,
+	{ createNewNote: createNote },
+)(App);
