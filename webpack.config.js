385fb656f530e692f1c72a13d06640b9f9b9cd2a
Niall Quigley <niallquigley92@gmail.com>
2019-08-13 18:43:25 +0100

    Add env variables to set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,56 @@
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-	entry: ['babel-polyfill', './src/index.js'],
-	resolve: {
-		extensions: ['.js', 'jsx'],
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.scss$/,
-				use: [
-					'style-loader', // creates style nodes from JS strings
-					'css-loader', // translates CSS into CommonJS
-					'sass-loader', // compiles Sass to CSS, using Node Sass by default
-				],
-			},
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-				},
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: 'html-loader',
-					},
-				],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-		}),
-	],
-};
+module.exports = () =>  {
+  const env = dotenv.config().parsed;
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    entry: ['babel-polyfill', './src/index.js'],
+    resolve: {
+      extensions: ['.js', 'jsx'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader', // creates style nodes from JS strings
+            'css-loader', // translates CSS into CommonJS
+            'sass-loader', // compiles Sass to CSS, using Node Sass by default
+          ],
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new webpack.DefinePlugin(envKeys)
+    ],
+  }
+}
