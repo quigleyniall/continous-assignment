e17a6473b83e7aa75c9e39389a307e081a5a619d
Niall Quigley <niallquigley92@gmail.com>
2019-08-13 17:32:19 +0100

    Fetch note

--- src/redux/reducers/notes.js
+++ src/redux/reducers/notes.js
@@ -9,36 +9,37 @@ import {
 type Note = {
 	id: number,
 	title: string,
-	note: string,
+	note?: string,
 };
 
 type State = {
-	notes: Array<Note>,
+	notes: Array<Note> | [],
 	selectedNote: Note | null,
-	selectedNoteId: number | null,
 	error: boolean | string,
 };
 
 const initialState = {
 	notes: [],
 	selectedNote: null,
-	selectedNoteId: null,
 	error: false,
 };
 
 export const notesReducer = (state: State = initialState, action) => {
 	switch (action.type) {
 		case fetchAllNotes:
-			return { ...state, notes: action.payload, error: false };
-		case fetchNote:
 			return {
 				...state,
-				selectedNote: state.notes.filter(
-					note => note.id === action.payload,
-				),
+				notes: action.payload,
+				error: false,
 			};
+		case fetchNote:
+			return { ...state, selectedNote: action.payload };
 		case createNewNote:
-			return { ...state, error: false };
+			return {
+				...state,
+				selectedNote: null,
+				error: false,
+			};
 		case errorHandler:
 			return { ...state, error: action.payload };
 		case updateNote:
