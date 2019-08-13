705b497b84316f77c4a1eb0e644222d4c3395e13
Niall Quigley <niallquigley92@gmail.com>
2019-08-13 17:31:25 +0100

    Update types

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -1,5 +1,6 @@
 export const fetchAllNotes = 'FETCH_ALL_NOTES';
 export const fetchNote = 'FETCH_NOTE';
+export const saveNewNote = 'SAVE_NEW_NOTE';
 export const createNewNote = 'CREATE_NEW_NOTE';
 export const updateNote = 'UPDATE_NOTE';
 export const errorHandler = 'ERROR_HANDLER';
